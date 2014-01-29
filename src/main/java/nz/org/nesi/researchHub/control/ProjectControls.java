e1a6a43b43fe66d08af55d22f3b6eba654155bd9
Nick <nick.young@auckland.ac.nz>
2014-01-29 13:58:47 +1300

    fix typo :\

--- src/main/java/nz/org/nesi/researchHub/control/ProjectControls.java
+++ src/main/java/nz/org/nesi/researchHub/control/ProjectControls.java
@@ -551,8 +551,8 @@ public class ProjectControls extends AbstractControl {
 			ProjectProperty old = this.projectDao.getProjectProperty(p.getId());
 			if (p.getPropname()!=null) old.setPropname(p.getPropname());
 			if (p.getPropvalue()!=null) old.setPropvalue(p.getPropvalue());
-			if (p.getSiteId()!=null) old.setPropvalue(p.getPropvalue());
-			if (p.getFacilityId()!=null) old.setPropvalue(p.getPropvalue());
+			if (p.getSiteId()!=null) old.setSiteId(p.getSiteId());
+			if (p.getFacilityId()!=null) old.setFacilityId(p.getFacilityId());
 			p = old;
 		}
     	this.projectDao.upsertProjectProperty(p);
