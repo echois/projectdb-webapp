1fe1cec448fcca39678d2afe09eb8ee96b466099
echois <eva.chois@gmail.com>
2014-03-24 12:16:45 +1300

    Added getAffiliationsByDivisionCode and institutionCode, getDepartmentsByDivisionCode, getDivisionsByInstitutionCode and getAffiliationStrings by request
    Some files are Reformatted by code formating script

--- src/main/java/nz/org/nesi/researchHub/control/ListControls.java
+++ src/main/java/nz/org/nesi/researchHub/control/ListControls.java
@@ -3,8 +3,10 @@
  */
 package nz.org.nesi.researchHub.control;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import nz.org.nesi.researchHub.exceptions.DatabaseException;
 import nz.org.nesi.researchHub.exceptions.InvalidEntityException;
@@ -23,7 +25,7 @@ public class ListControls extends AbstractControl {
 	/**
 	 * 
 	 */
-	private static final String SEPERATOR = " -- ";
+	private static final String SEPARATOR = " -- ";
 
 	public static void main(final String[] args) throws Exception {
 
@@ -38,7 +40,7 @@ public class ListControls extends AbstractControl {
 		}
 
 	}
-	// Read
+
 	/**
 	 * Returns list of all affiliations.
 	 * 
@@ -55,16 +57,146 @@ public class ListControls extends AbstractControl {
 		return afl;
 
 	}
-	
-	public void createAffiliation(final Affiliation affiliation) throws InvalidEntityException {
+
+	/**
+	 * Returns list of affiliations on specific institution.
+	 * 
+	 * @return affiliations on specific institution in the project database
+	 */
+	public List<Affiliation> getAffiliationsByInstitutionCode(
+			String institutionCode) {
+
+		List<Affiliation> afl = null;
+		try {
+			afl = projectDao.getAffiliationsByInstitutionCode(institutionCode);
+		} catch (final Exception e) {
+			throw new DatabaseException("Can't get Affiliations.", e);
+		}
+		return afl;
+	}
+
+	/**
+	 * Returns list of affiliations in string.
+	 * 
+	 * @return affiliations in string in the project database
+	 */
+	public List<String> getAffiliationStrings() throws Exception {
+		final List<Affiliation> affiliations = projectDao.getAffiliations();
+		final List<String> affiliationStrings = new LinkedList<String>();
+		if (affiliations != null) {
+			for (final Affiliation a : affiliations) {
+				String tmp = a.getInstitution().trim();
+				if (!a.getDivision().trim().isEmpty()) {
+					tmp += SEPARATOR + a.getDivision().trim();
+					if (!a.getDepartment().trim().isEmpty()) {
+						tmp += SEPARATOR + a.getDepartment().trim();
+					}
+				}
+				affiliationStrings.add(tmp);
+			}
+		}
+		return affiliationStrings;
+	}
+
+	/**
+	 * Returns list of division and division codes on specific institution.
+	 * 
+	 * @return division and division codes on specific institution in the
+	 *         project database
+	 */
+
+	public List<Map<String, String>> getDivisionsByInstitutionCode(
+			String institutionCode) {
+
+		final List<Affiliation> affiliations = getAffiliationsByInstitutionCode(institutionCode);
+		final List<Map<String, String>> divisionStrings = new LinkedList<Map<String, String>>();
+		if (affiliations != null) {
+			for (final Affiliation a : affiliations) {
+				String di = "";
+				String diCode = "";
+				Map<String, String> divisionInfo = new HashMap<String, String>();
+				if (!a.getDivision().isEmpty()
+						&& !a.getDivisionCode().isEmpty()) {
+					di += a.getDivision();
+					diCode += a.getDivisionCode();
+					divisionInfo.put(di, diCode);
+				}
+				divisionStrings.add(divisionInfo);
+			}
+		}
+		return divisionStrings;
+	}
+
+	/**
+	 * Returns list of affiliations on specific division.
+	 * 
+	 * @return affiliations on specific division in the project database
+	 */
+	public List<Affiliation> getAffiliationsByDivisionCode(String divisionCode) {
+
+		List<Affiliation> afl = null;
+		try {
+			afl = projectDao.getAffiliationsByDivisionCode(divisionCode);
+		} catch (final Exception e) {
+			throw new DatabaseException("Can't get Affiliations.", e);
+		}
+		return afl;
+
+	}
+
+	/**
+	 * Returns list of department and department codes on specific division.
+	 * 
+	 * @return department and department codes on specific division in the
+	 *         project database
+	 */
+
+	public List<Map<String, String>> getDepartmentsByDivisionCode(
+			String divisionCode) {
+
+		final List<Affiliation> affiliations = getAffiliationsByDivisionCode(divisionCode);
+		final List<Map<String, String>> departmentStrings = new LinkedList<Map<String, String>>();
+		if (affiliations != null) {
+			for (final Affiliation a : affiliations) {
+				String de = "";
+				String deCode = "";
+				Map<String, String> departmentInfo = new HashMap<String, String>();
+				if (!a.getDepartment().isEmpty()
+						&& !a.getDepartmentCode().isEmpty()) {
+					de += a.getDepartment();
+					deCode += a.getDepartmentCode();
+					departmentInfo.put(de, deCode);
+				}
+				departmentStrings.add(departmentInfo);
+			}
+		}
+		return departmentStrings;
+	}
+
+	/**
+	 * Creates affiliation new affiliation.
+	 * 
+	 * The Affiliation object can't have an id specified, since that gets
+	 * auto-generated at affiliation lower level.
+	 * 
+	 * @param affiliation
+	 *            the new Affiliation
+	 * @throws InvalidEntityException
+	 *             if the new Affiliation object has already an id specified
+	 */
+	public void createAffiliation(final Affiliation affiliation)
+			throws InvalidEntityException {
 		try {
 			projectDao.createInstitution(affiliation);
-			if(!affiliation.getDivision().isEmpty() ||!affiliation.getDivisionCode().isEmpty())
-				projectDao.createDivision(affiliation);
-			if(!affiliation.getDepartment().isEmpty()||!affiliation.getDepartmentCode().isEmpty())			
-				projectDao.createDepartment(affiliation);
+			if (!affiliation.getDivision().isEmpty()
+					|| !affiliation.getDivisionCode().isEmpty()) projectDao
+					.createDivision(affiliation);
+			if (!affiliation.getDepartment().isEmpty()
+					|| !affiliation.getDepartmentCode().isEmpty()) projectDao
+					.createDepartment(affiliation);
 		} catch (final Exception e) {
-			throw new DatabaseException("Can't create affiliation '"+ affiliation.getInstitution() + "'", e);
+			throw new DatabaseException("Can't create affiliation '"
+					+ affiliation.getInstitution() + "'", e);
 		}
 	}
 }
