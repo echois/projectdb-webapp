32ee2a63b95600ccee05edcfa4fbde3ec2f28e15
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2014-06-12 16:30:41 +1200

    disable csrf check

--- src/main/java/nz/org/nesi/researchHub/auth/AuthConfig.java
+++ src/main/java/nz/org/nesi/researchHub/auth/AuthConfig.java
@@ -3,6 +3,7 @@ package nz.org.nesi.researchHub.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +14,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 @EnableWebMvcSecurity
 public class AuthConfig extends WebSecurityConfigurerAdapter {
 
+
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .csrf().disable();
+  }
 
 	@Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
